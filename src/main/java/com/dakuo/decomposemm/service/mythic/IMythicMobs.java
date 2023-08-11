45aaef0b7c1b38af81f776d460ffb0312b8d8a84
CoderKuo <1520622465@qq.com>
2023-08-11 09:40:48 +0800

    [+] Mythic

---
+++ src/main/java/com/dakuo/decomposemm/service/mythic/IMythicMobs.java
@@ -0,0 +1,7 @@
+package com.dakuo.decomposemm.service.mythic;
+
+import org.bukkit.inventory.ItemStack;
+
+public interface IMythicMobs {
+    public ItemStack getItem(String id);
+}
---
+++ src/main/java/com/dakuo/decomposemm/service/mythic/MythicMobs4.java
@@ -0,0 +1,12 @@
+package com.dakuo.decomposemm.service.mythic;
+
+import org.bukkit.inventory.ItemStack;
+
+public class MythicMobs4 implements IMythicMobs{
+
+    @Override
+    public ItemStack getItem(String id) {
+
+        return null;
+    }
+}
